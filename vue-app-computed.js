eacd600f0cb2468bb11a8f20c73e4b50d7aa894c
Pulipuli Chen <blog@pulipuli.info>
2020-05-21 01:59:19 +0800

    0521 #8 暫存

--- vue-app-computed.js
+++ vue-app-computed.js
@@ -9,26 +9,72 @@ var appComputed = {
     },
     sortedTopicDocuments () {
       
-      setTimeout(() => {
-        let table = $(this.$refs.DocumentTable)
-        table.find('.sentence').popup()
-        table.tablesort({
-          sortInitialOrder: 'desc',
-          afterSort: () => {
-            table.find('.sentence').popup()
-          }
-        })
-      }, 0)
-      if (this.sortDocuments === -1) {
-        //console.log(this.topicDocuments[0])
+      if (this.documentSortField === null) {
         return this.topicDocuments
       }
-      else {
-        let output = JSON.parse(JSON.stringify(this.topicDocuments))
-        output.sort(function (a, b) {
-          return (b.theta[this.sortDocuments] - a.theta[this.sortDocuments])
+      
+      let sortDocuments = Array.from(Object.create(this.topicDocuments));
+      //console.log(this.documentSortField)
+      if (this.documentSortField === 'id') {
+        sortDocuments.sort((a, b) => {
+          if (this.documentSortOrder === 'asc') {
+            return (a.id - b.id)
+          }
+          else {
+            return (b.id - a.id)
+          }
         })
       }
+      else if (this.documentSortField === 'topics') {
+        sortDocuments.sort((a, b) => {
+          if (this.documentSortOrder === 'asc') {
+            return (a.topic - b.topic)
+          }
+          else {
+            return (b.topic - a.topic)
+          }
+        })
+      }
+      else if (this.documentSortField === 'alphabetical') {
+        sortDocuments.sort((a, b) => {
+          let sort = -1
+          if (this.documentSortOrder === 'asc') {
+            if (a.sentence > b.sentence) {
+              sort = 1
+            }
+            else {
+              sort = -1
+            }
+          }
+          else {
+            if (a.sentence > b.sentence) {
+              sort = -1
+            }
+            else {
+              sort = 1
+            }
+          }
+          return sort
+        })
+      }
+      else if (typeof(this.documentSortField) === 'number') {
+        let i = this.documentSortField
+        //console.log(i)
+        sortDocuments.sort((a, b) => {
+          if (this.documentSortOrder === 'asc') {
+            return (a.theta[i] - b.theta[i])
+          }
+          else {
+            return (b.theta[i] - a.theta[i])
+          }
+        })
+      }
+      //console.log(this.documentSortField)
+      
+      return sortDocuments
+    },
+    sortedTopicTerms () {
+      return []
     },
     topicNumberArray () {
       let output = []
